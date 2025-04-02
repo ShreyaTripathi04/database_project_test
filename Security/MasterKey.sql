198891023bb834eedac9b0c844aa0e5f1ed6c9b5
Shreya Tripathi <Shreya.Tripathi@wsp.com>
2025-04-02 10:19:28 +0530

    added

---
+++ Security/MasterKey.sql
@@ -0,0 +1,15 @@
+﻿/*
+Post-Deployment Script Template							
+--------------------------------------------------------------------------------------
+ This file contains SQL statements that will be appended to the build script.		
+ Use SQLCMD syntax to include a file in the post-deployment script.			
+ Example:      :r .\myfile.sql								
+ Use SQLCMD syntax to reference a variable in the post-deployment script.		
+ Example:      :setvar TableName MyTable							
+               SELECT * FROM [$(TableName)]					
+--------------------------------------------------------------------------------------
+*/
+IF NOT EXISTS (SELECT * FROM sys.symmetric_keys WHERE name = '10EB4200-DCB0-47A9-8378-BF7FA70E574B')
+BEGIN
+    CREATE MASTER KEY ENCRYPTION BY PASSWORD = 'Shreya@12345';
+END
---
+++ dbo/Tables/TestTable.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[TestTable] (
+    [ID]        INT           IDENTITY (1, 1) NOT NULL,
+    [Name]      NVARCHAR (50) NOT NULL,
+    [CreatedAt] DATETIME      DEFAULT (getdate()) NULL,
+    [Status]    BIT           DEFAULT ((1)) NULL,
+    PRIMARY KEY CLUSTERED ([ID] ASC)
+);
+
